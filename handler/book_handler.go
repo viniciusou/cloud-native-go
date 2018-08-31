3e7648da27b9eb54e9148ea76e5c7ebb6c572c80
Vinicius Uemura <viniciusou@gmail.com>
2018-08-31 00:11:30 -0300

    Method POST All books

--- handler/book_handler.go
+++ handler/book_handler.go
@@ -2,6 +2,8 @@ package handler
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/viniciusou/cloud-native-go/model"
@@ -14,6 +16,19 @@ func BooksHandleFunc(w http.ResponseWriter, r *http.Request) {
 	case http.MethodGet:
 		books := AllBooks()
 		writeJSON(w, books)
+	case http.MethodPost:
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		book := readJSON(body)
+		isbn, created := CreateBook(book)
+		if created {
+			w.Header().Add("location", "/api/books/"+isbn)
+			w.WriteHeader(http.StatusCreated)
+		} else {
+			w.WriteHeader(http.StatusConflict)
+		}
 	default:
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte("Unsupported request method"))
@@ -32,6 +47,17 @@ func AllBooks() []model.Book {
 	return values
 }
 
+//CreateBook creates a new book if it does not exist
+func CreateBook(book model.Book) (string, bool) {
+	_, exists := repository.Books[book.ISBN]
+	if exists {
+		return "", false
+	}
+
+	repository.Books[book.ISBN] = book
+	return book.ISBN, true
+}
+
 func writeJSON(w http.ResponseWriter, i interface{}) {
 	byteSlice, err := json.Marshal(i)
 	if err != nil {
@@ -41,4 +67,14 @@ func writeJSON(w http.ResponseWriter, i interface{}) {
 
 	w.Header().Add("Content-Type", "application/json: charset=utf-8")
 	w.Write(byteSlice)
+}
+
+func readJSON(data []byte) model.Book {
+	book := model.Book{}
+	err := json.Unmarshal(data, &book)
+	if err != nil {
+		fmt.Println("Error to deserialize JSON", err.Error())
+	}
+
+	return book
 }
